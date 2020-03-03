e5cba4f4465ebd7569bef41750573ec10fc9d0f4
ashwin-shekher <61041894+ashwin-shekher@users.noreply.github.com>
2020-03-03 16:25:18 +0530

    code corrections

--- app/routes/app-detail.js
+++ app/routes/app-detail.js
@@ -8,22 +8,28 @@ import { tracked } from "@glimmer/tracking";
 
 export default class AppDetailRoute extends Route {
   @service store;
-  params;
-  @tracked download_count;
+  @tracked params;
 
   async model(params) {
     this.params = params;
-    return this.store.peekRecord("app-data", params.id);
+    let store = this.store.peekRecord("app-data", params.id);
+    if (!store) {
+      await this.store.findAll("app-data");
+      return this.store.peekRecord("app-data", params.id);
+    } else {
+      return store;
+    }
   }
 
   @action downloadNow() {
+    let params = this.params;
     this.store
-      .findRecord("app-data", this.params.id)
+      .findRecord("app-data", params.id)
       .then(function(appData) {
         appData.download_count = appData.download_count + 1;
       })
       .catch(err => {
-    
+        console.log(err);
       });
   }
 }
